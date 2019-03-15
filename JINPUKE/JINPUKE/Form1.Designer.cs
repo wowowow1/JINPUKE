ee15f3ddcb1003525308138e1c5b1d78737c38a5
My Home <786095034@qq.com>
2019-03-15 22:31:01 +0800

    1、my JINPUKE game

---
+++ JINPUKE/JINPUKE/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace JINPUKE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.Location = new System.Drawing.Point(312, 476);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(186, 77);
+            this.btnDeal.TabIndex = 0;
+            this.btnDeal.Text = "发牌";
+            this.btnDeal.UseVisualStyleBackColor = true;
+            this.btnDeal.Click += new System.EventHandler(this.btnDeal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(507, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(660, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(143, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(266, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(373, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(510, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 15);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(663, 206);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 15);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 710);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ JINPUKE/JINPUKE/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JINPUKE
+{
+    
+    public partial class Form1 : Form
+    {
+        readonly int[] poker = new int[52] { 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414,
+                                          302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 
+                                          202, 203, 204, 205, 306, 207, 208, 209, 210, 211, 212, 213, 214,
+                                          102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114 };
+                                         /* 百位4-黑桃,3-红桃,2-草花,1-方块;十位+个位：2-10对应2-10,11-13对应J-K,14-A */
+        readonly string[] pokerSuit = new string[4] { "方块", "草花", "红桃", "黑桃" };
+        readonly string[] pokerNum = new string[13] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 获取若干个不相同int型随机数
+        /// </summary>
+        /// <param name="num">要获取的随机数数量</param>
+        /// <param name="minValue">随机数区间的最小值</param>
+        /// <param name="maxValue">随机数区间的最大值</param>
+        /// <returns></returns>
+        public int[] getRandomNum(int num, int minValue, int maxValue)         ///////////此处可能存在bug需测试，可能会产生相同随机数！！！
+        {
+            Random ra = new Random(unchecked((int)DateTime.Now.Ticks));
+            int[] arrNum = new int[num];
+            int tmp = 0;
+            for (int i = 0; i <= num - 1; i++)
+            {
+                tmp = ra.Next(minValue, maxValue); //随机取数
+                arrNum[i] = getNum(arrNum, tmp, minValue, maxValue, ra); //取出值赋到数组中
+            }
+            return arrNum;
+        }
+        public int getNum(int[] arrNum, int tmp, int minValue, int maxValue, Random ra)
+        {
+            int n = 0;
+            while (n <= arrNum.Length - 1)
+            {
+                if (arrNum[n] == tmp)
+                {
+                    tmp = ra.Next(minValue, maxValue);
+                    getNum(arrNum, tmp, minValue, maxValue, ra);
+                }
+                n++;
+            }
+            return tmp;
+        }
+
+        private void btnDeal_Click(object sender, EventArgs e)
+        {
+            int[] pokerRandom = new int[5];
+            
+            pokerRandom = getRandomNum(5, 1, 53);
+
+            label1.Text = pokerNameCalculate(poker[pokerRandom[0] - 1]);
+            label2.Text = pokerNameCalculate(poker[pokerRandom[1] - 1]);
+            label3.Text = pokerNameCalculate(poker[pokerRandom[2] - 1]);
+            label4.Text = pokerNameCalculate(poker[pokerRandom[3] - 1]);
+            label5.Text = pokerNameCalculate(poker[pokerRandom[4] - 1]);
+
+            label6.Text = poker[pokerRandom[0] - 1].ToString();
+            label7.Text = poker[pokerRandom[1] - 1].ToString();
+            label8.Text = poker[pokerRandom[2] - 1].ToString();
+            label9.Text = poker[pokerRandom[3] - 1].ToString();
+            label10.Text = poker[pokerRandom[4] - 1].ToString();
+
+
+            if((label1.Text == label2.Text) || (label1.Text == label3.Text) || (label1.Text == label4.Text) || (label1.Text == label5.Text))
+            {
+                btnDeal.Enabled = false;
+            }
+            else
+            {
+                if ((label2.Text == label3.Text) || (label2.Text == label4.Text) || (label2.Text == label5.Text))
+                {
+                    btnDeal.Enabled = false;
+                }
+                else
+                {
+                    if ((label3.Text == label4.Text) || (label3.Text == label5.Text))
+                    {
+                        btnDeal.Enabled = false;
+                    }
+                    else
+                    {
+                        if (label4.Text == label5.Text)
+                        {
+                            btnDeal.Enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+        private string pokerNameCalculate(int pokerCode)
+        {
+            string pokerName, sPokerNum;
+            int _pokerSuit = pokerCode / 100;
+            int _pokerNum = pokerCode % 100;
+            if (_pokerNum < 14)
+            {
+                sPokerNum = pokerNum[_pokerNum - 2];  // 2~K
+            }
+            else
+            {
+                sPokerNum = pokerNum[12];// A
+            }
+            pokerName = pokerSuit[_pokerSuit - 1] + sPokerNum;
+            return pokerName;
+        }
+
+        
+    }
+}
